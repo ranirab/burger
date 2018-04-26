5f0bb6d4433300e4b1d0a216073a71065abb2d57
Rani Rab <rani@Ranis-MacBook-Pro.local>
2018-04-25 20:01:45 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # burger
+A burger logger with MySQL, Node, Express, Handlebars and a homemade ORM using MVC design pattern.  Node and MySQL to query and route data, and Handlebars to generate HTML.
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name, devoured) VALUES ('cheese burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('pimiento burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('jalapeno burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('veggie burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('black bean burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('mushroom burger', true);
